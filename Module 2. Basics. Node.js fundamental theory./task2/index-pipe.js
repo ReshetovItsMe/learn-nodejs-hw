750e737e126aa57e49ce4d019aaa4eaf8532e8e7
Vlad Reshetov <reshetovitsme@gmail.com>
2021-03-09 13:15:14 +0300

    [Module 2] Task 2 on pipeline by line

--- Module 2. Basics. Node.js fundamental theory./task2/index-pipe.js
+++ Module 2. Basics. Node.js fundamental theory./task2/index-pipe.js
@@ -1,11 +1,13 @@
 const csv = require('csvtojson');
 const fs = require('fs');
+const byline = require('byline');
 
 const inputFilePath = process.argv[2]; // input file from npm task argument
 const outputFilePath = process.argv[3]; // input file from npm task argument
 
+const readStream = fs.createReadStream(inputFilePath).pipe(csv());
+const lineReadStream = byline.createStream(readStream)
 const writeStream = fs.createWriteStream(outputFilePath);
-const readStream = fs.createReadStream(inputFilePath);
 
 readStream.on('error', (err) => {
   console.log('Error in read stream...', err);
@@ -14,4 +16,4 @@ writeStream.on('error', (err) => {
   console.log('Error in write stream...', err);
 });
 
-readStream.pipe(csv()).pipe(writeStream);
+lineReadStream.pipe(writeStream);
--- Module 2. Basics. Node.js fundamental theory./task2/index.js
+++ Module 2. Basics. Node.js fundamental theory./task2/index.js
@@ -2,15 +2,20 @@
 const csv = require('csvtojson');
 const fs = require('fs');
 const { pipeline } = require('stream');
+const byline = require('byline');
 
 const inputFilePath = process.argv[2]; // input file from npm task argument
 const outputFilePath = process.argv[3]; // input file from npm task argument
 
-pipeline(fs.createReadStream(inputFilePath).pipe(csv()), fs.createWriteStream(outputFilePath),
+const readStream = fs.createReadStream(inputFilePath).pipe(csv());
+const lineStream = byline.createStream(readStream);
+const writeStream = fs.createWriteStream(outputFilePath);
+
+pipeline(lineStream, writeStream,
   (err) => {
     if (err) {
       console.error('Pipeline failed.', err);
     } else {
       console.log('Pipeline succeeded.');
     }
-  });
+  })
