31845422c6b5be9af1f14e161f0947b804ee5fcd
Vlad Reshetov <reshetovitsme@gmail.com>
2021-03-09 10:57:53 +0300

    [Module 2] Task 2 on pipes

--- Module 2. Basics. Node.js fundamental theory./task2/index.js
+++ Module 2. Basics. Node.js fundamental theory./task2/index.js
@@ -1,12 +1,12 @@
 const csv = require('csvtojson');
-const readline = require('readline');
 const fs = require('fs');
 
 const inputFilePath = process.argv[2]; // input file from npm task argument
 const outputFilePath = process.argv[3]; // input file from npm task argument
 
-const readStream = fs.createReadStream(inputFilePath).pipe(csv());
-const writeStream = fs.createWriteStream(outputFilePath)
+const writeStream = fs.createWriteStream(outputFilePath);
+const readStream = fs.createReadStream(inputFilePath);
+
 readStream.on('error', (err) => {
   console.log('Error in read stream...', err);
 });
@@ -14,14 +14,4 @@ writeStream.on('error', (err) => {
   console.log('Error in write stream...', err);
 });
 
-const rl = readline.createInterface({
-  input: readStream
-});
-
-rl.on('line', (line) => {
-  writeStream.write(`${line}\n`);
-});
-
-rl.on('close', () => {
-  writeStream.close()
-})
+readStream.pipe(csv()).pipe(writeStream);
--- Module 2. Basics. Node.js fundamental theory./task3/src/task2.js
+++ Module 2. Basics. Node.js fundamental theory./task3/src/task2.js
@@ -1,12 +1,12 @@
 import csv from 'csvtojson';
-import readline from 'readline';
 import fs from 'fs';
 
 const inputFilePath = process.argv[2]; // input file from npm task argument
 const outputFilePath = process.argv[3]; // input file from npm task argument
 
-const readStream = fs.createReadStream(inputFilePath).pipe(csv());
-const writeStream = fs.createWriteStream(outputFilePath)
+const writeStream = fs.createWriteStream(outputFilePath);
+const readStream = fs.createReadStream(inputFilePath);
+
 readStream.on('error', (err) => {
   console.log('Error in read stream...', err);
 });
@@ -14,14 +14,4 @@ writeStream.on('error', (err) => {
   console.log('Error in write stream...', err);
 });
 
-const rl = readline.createInterface({
-  input: readStream
-});
-
-rl.on('line', (line) => {
-  writeStream.write(`${line}\n`);
-});
-
-rl.on('close', () => {
-  writeStream.close()
-})
+readStream.pipe(csv()).pipe(writeStream);
